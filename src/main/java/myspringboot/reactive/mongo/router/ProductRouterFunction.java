a79a2fd1a332c899877da5608a3d81002c0668f9
mysoyul <vega2k_@naver.com>
2021-12-21 16:32:00 +0900

    router handler

--- src/main/java/myspringboot/reactive/mongo/router/ProductRouterFunction.java
+++ src/main/java/myspringboot/reactive/mongo/router/ProductRouterFunction.java
@@ -7,7 +7,7 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -17,6 +17,9 @@ public class ProductRouterFunction {
     public RouterFunction<ServerResponse> routerFunction(ProductHandler productHandler) {
         return route(GET("/router/products"),productHandler::getProducts)
                 .andRoute(GET("/router/products/{id}"),productHandler::getProduct)
-                .andRoute(GET("/router/product-range"), productHandler::getProductInRange);
+                .andRoute(GET("/router/product-range"), productHandler::getProductInRange)
+                .andRoute(POST("/router/products"),productHandler::saveProduct)
+                .andRoute(PATCH("/router/products/{id}"),productHandler::updateProduct)
+                .andRoute(DELETE("/router/products/{id}"),productHandler::deleteProduct);
     }
 }
